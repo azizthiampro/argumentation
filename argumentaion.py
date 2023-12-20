fbdf3eba24778f03bdb081b02aef5355014abbc1
azizthiampro <azizthiampro@gmail.com>
2023-12-20 15:14:44 +0100

    Final

--- argumentaion.py
+++ argumentaion.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import itertools
+from itertools import permutations
+
 
 
 arguments = set()
@@ -32,20 +34,7 @@ def is_attacked(arg, attacks):
     return any(x[1] == arg for x in attacks)
 
 def get_arg_attackers(arg, attacks):
-	"""
-	Given an argument "arg" and the attack relations "attacks" return a set
-	of arguments that attacks "arg".
 
-	Parameters
-	----------
-	arg (str or int): An argument
-	attacks (list of 2-uples): Attack relations
-
-	Returns
-	-------
-	Set: Set of arguments that attacks "arg".
-
-	"""
 	attackers = set()
 	for i in attacks:
 		if i[1] == arg:
@@ -53,20 +42,7 @@ def get_arg_attackers(arg, attacks):
 	return attackers
 
 def get_attacked_args(set_of_args, attacks):
-	"""
-	Given a set of arguments "set_of_args" and the attack relations "attacks"
-	return the set of arguments that "set_of_args" attacks.
 
-	Parameters
-	----------
-	set_of_args (set): Set of arguments
-	attacks (list of 2-uples): Attack relations
-
-	Returns
-	-------
-	Set: Set of arguments that "set_of_args" attacks.
-
-	"""
 	attacked = set()
 	for i in attacks:
 		if i[0] in set_of_args:
@@ -76,43 +52,13 @@ def get_attacked_args(set_of_args, attacks):
 
 
 def powerset(iterable):
-	"""
-	Powerset.
 
-	Parameters
-	----------
-	iterable (list or set): Set of arguments
-
-	Returns
-	-------
-	Set: Subsets of arguments
-
-	"""
 	s = list(iterable)
 	return set(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s) + 1)))
 
 
 def compute_acceptability(arg, E, relations):
-	"""
-	Return if the argument "arg" is acceptable or not respect the set of 
-	arguments "E".
-	
-	An argument "arg" in AF is ACCEPTABLE with respect to "E"
-	(subset de AF) if and only if "E" defends "arg", that is, forall "b" in AF
-	such that (b,arg) in R, exists c in E such that (c,b) in R.
 
-	Parameters
-	----------
-	arg (str or int): An argument
-	E (set): A set of arguments
-	relations (list of 2-uples): Attack relations
-
-	Returns
-	-------
-	Bool: if the argument "arg" is acceptable or not respect the set of 
-	arguments "E".
-
-	"""
 	attackers = get_arg_attackers(arg, relations)
 	if attackers != None:
 		atks = []
@@ -129,19 +75,7 @@ def compute_acceptability(arg, E, relations):
 
 
 def checkArgumentsInRelations(arguments, relations):
-	"""
-	Check if arguments in relations exist in arguments
 
-	Parameters
-	----------
-	arguments (set) : Set of arguments
-	relations (list of 2-uples): Attack relations
-
-	Returns
-	-------
-	Bool: True if all arguments in relations exist in arguments, else False
-
-	"""
 	if len(arguments) > 0:
 		if len(relations) > 0:
 			for x in relations:
@@ -161,58 +95,17 @@ def checkArgumentsInRelations(arguments, relations):
 
 
 class Extensions:
-	"""
-    Class used to represent extensions
 
-    ...
-
-    Attributes
-    ----------
-    arguments (set): Set of arguments
-    extensions (set): Attack relations
-
-    Methods
-    -------
-    get_Extensions()
-        Return extensions
-	get_SkepticallyAcceptedArguments()
-		Return skeptiacally accepted arguments
-	get_CredulouslyAcceptedArguments()
-		Return credulously accepted arguments
-	
-    """
 	def __init__(self, extensions, arguments):
 		self.__extensions = extensions
 		self.__arguments = arguments
 
 	def get_Extensions(self):
-		"""
-		Return extensions
 
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		Set: Extensions
-
-		"""
 		return self.__extensions
 		
 	def get_SkepticallyAcceptedArguments(self):
-		"""
-		Return set of skeptically accepted arguments
 
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		Set: Skeptically accepted arguments
-
-		"""
 		accepted = set()
 		if len(self.__extensions) > 0:
 			for a in self.__arguments:
@@ -227,18 +120,7 @@ class Extensions:
 		return accepted
 
 	def get_CredulouslyAcceptedArguments(self):
-		"""
-		Return set of credulously accepted arguments
 
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		Set: Credulously accepted arguments
-
-		"""
 		accepted = set()
 		if len(self.__extensions) > 0:
 			for a in self.__arguments:
@@ -251,42 +133,14 @@ class Extensions:
 
 
 class Dung:
-	"""
-    Class used to represent a Dung Argumentation Framework
 
-    ...
-
-    Attributes
-    ----------
-    arguments (set): Set of arguments
-    relations (set): Attack relations
-    semantics (Class Semantics): Acceptability semantics
-
-    Methods
-    -------
-    compute_cfs()
-        Compute conflic-free subsets of arguments
-	compute_admissibility()
-		Compute abmissible set of arguments
-    """
 	def __init__(self, arguments, relations):
 		self.__arguments = arguments
 		self.__relations = relations
 		self.semantics = Dung.Semantics(self)
 
 	def compute_cfs(self):
-		"""
-		Return conflic-free subsets
 
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		Set: Conflict-free subsets
-
-		"""
 		args = self.__arguments
 		rel = self.__relations
 
@@ -310,21 +164,6 @@ class Dung:
 
 
 	def compute_admissibility(self):
-		"""
-		For each conflict free subset, if its attackers are
-		attacked (exhaustively) by a subset of that cfs, it is admissible
-		A conglict-free set of arguments S is ADMISIBLE iff each argument
-		in S is ACCEPTABLE with respect to S.
-
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		Set: Admissible arguments subsets
-
-		"""
 		cfs = self.compute_cfs()
 
 		rel = self.__relations
@@ -365,20 +204,7 @@ class Dung:
 			self.af = af
 
 		def compute_stable_extensions(self):
-			"""
-			E is a stable extension of AF only if it is a conflict-free set that attacks 
-			every argument that does not belong in E (formally, forall a in A\E, exists 
-			b in E forall a in A\E,exists b in E such that (b,a) in R(b,a) in R.
 
-			Parameters
-			----------
-			None
-
-			Returns
-			-------
-			Set: Stable extensions subsets
-
-			"""
 			if checkArgumentsInRelations(self.af._Dung__arguments, self.af._Dung__relations) == True:
 				adm = self.af.compute_cfs()
 				stb = []
@@ -393,19 +219,7 @@ class Dung:
 
 		
 		def compute_complete_extensions(self):
-			"""
-			An admissible set S of arguments is called a complete extension iff
-			each argument, which is acceptable with respect to S, belongs to S.
-
-			Parameters
-			----------
-			None
-
-			Returns
-			-------
-			Set: complete extensions subsets
-
-			"""
+	
 			if checkArgumentsInRelations(self.af._Dung__arguments, self.af._Dung__relations)==True:
 				compl = []
 				adm = self.af.compute_admissibility()
@@ -431,38 +245,64 @@ def decide(elem, arg,set1):
             print("NO")
     else:
         print("Argument not known")
+        
 
-
-def verify(set1, arg, bigset):
+def verify_complete(set1, arg, bigset):
     for argument in set1:
         if argument not in arg:
             print(f"Error argument \"{argument}\"  not my arguments, Please change the argument name.")
             sys.exit(1) 
-    if set1 in bigset:
+    if set(set1) in bigset:
         print("YES")
     else:
         print("NO")
+
+
+def verify_stable(set1, arg, bigset):
+    for argument in set1:
+        if argument not in arg:
+            print(f"Error argument \"{argument}\"  not my arguments, Please change the argument name.")
+            sys.exit(1) 
+    if is_combination_in_list(set1, bigset):
+         print("YES")
+    else:
+         print("NO")
 
 def process_data(input_data):
     # Check if input_data contains commas
     if ',' in input_data:
         # If commas are present, split the data and store it in a tuple
-        dataDecide =sorted( tuple(map(str.strip, input_data.split(','))))
+        dataDecide = tuple(map(str.strip, input_data.split(',')))
     else:
         # If no commas, store the single alphanumeric word
         dataDecide = input_data.strip()
 
     return dataDecide
 
+def is_combination_in_list(check_tuple, tuple_list):
+    
+    for permuted_tuple in permutations(check_tuple):
+        if permuted_tuple in tuple_list:
+            return True
+    return False
 
 def main():
+    
+    problem_type = ""
+    file_path = ""
+    query_args = []
+    
     if len(sys.argv) < 5:
-        print("Usage: python test_af1.py -p [VE-CO | VE-ST | DC-CO | DS-CO | DC-ST | DS-ST] -f FILE -a ARG1,ARG2,...,ARGn")
+        print("Please enter a command like: python myfile.py -p [VE-CO | VE-ST | DC-CO | DS-CO | DC-ST | DS-ST] -f FILE -a ARG1,ARG2,...,ARGn")
         sys.exit(1)
+    else:
+         problem_type = sys.argv[2]
+         file_path = sys.argv[4]
+         query_args = sys.argv[6]
+    
+        
 
-    problem_type = sys.argv[2]
-    file_path = sys.argv[4]
-    query_args = sys.argv[6]
+ 
     
     if os.path.exists(file_path):
         try:
@@ -484,8 +324,6 @@ def main():
     
     st_ext=st.get_Extensions()
     co_ext=co.get_Extensions()
-
-    
     
     st_skep=st.get_SkepticallyAcceptedArguments()
 
@@ -498,31 +336,22 @@ def main():
     
     
     if problem_type == "VE-CO":
-       print("The complete one is : ",co_ext)
+        verify_complete(argument_or_set, arguments,co_ext)
 
-    elif problem_type == "VE-ST":
-        print("The stable one is : ",st_ext)
-       
+    elif problem_type == "VE-ST":  
+        verify_stable(argument_or_set, arguments,st_ext)
 
 
     elif problem_type in ["DC-CO", "DS-CO", "DC-ST", "DS-ST"]:
       
         if problem_type == "DC-CO":
-            print("The co_cred one is : ",co_cred)
             decide(argument_or_set, arguments,co_cred)
         elif problem_type == "DS-CO":
-            print("The co_skep one is : ",co_skep)
             decide(argument_or_set, arguments,co_skep)
         elif problem_type == "DC-ST":
-            print("The st_cred one is : ",st_cred)
             decide(argument_or_set, arguments,st_cred)
         elif problem_type == "DS-ST":
-            print("The st_skep one is : ",st_skep)
             decide(argument_or_set, arguments,st_skep)
- 
-    
-    
-        
 
 if __name__ == "__main__":
     main()
